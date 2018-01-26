b670564b4e60cd47e1cb3f287e92417ef304e14a
Anikesh <anikeshpatel4@gmail.com>
2018-01-26 11:39:41 +0530

    init launcher

---
+++ src/grp/anon/launcher/Launcher.java
@@ -0,0 +1,20 @@
+package grp.anon.launcher;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("launcher.fxml")));
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Phony - The Android Simulator");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
