57a16a8211ff3fec47aeeb4ae9a2d7c6b6b001cb
Anikesh <anikeshpatel4@gmail.com>
2018-01-26 17:23:53 +0530

    Calcy Design Done

---
+++ src/grp/anon/calcy/Calcy.java
@@ -0,0 +1,13 @@
+package grp.anon.calcy;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Calcy implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
